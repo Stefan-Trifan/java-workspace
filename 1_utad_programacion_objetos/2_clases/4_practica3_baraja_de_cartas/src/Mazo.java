052f3b1323d8a64b78abaede86000dbf6ac8fcc5
Stefan <trfstephan@gmail.com>
2026-02-06 10:24:19 +0100

    inicio ejercicio baraja de cartas

---
+++ 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/Mazo.java
@@ -0,0 +1,67 @@
+public class Mazo
+{
+    // _________________________________________
+    // _______________________________ Atributos 
+    // todo tiene 40 cartas (composicion)
+
+
+    // _________________________________________
+    // ___________________ Constructor Principal 
+
+    // _________________________________________
+    // ________________ Constructores Auxiliares 
+
+    // _________________________________________
+    // _________________________________ Getters
+
+    // _________________________________________
+    // _________________________________ Setters
+
+    // __________________________________________
+    // _________________________ Métodos Públicos 
+    // Cambia de posición todas las cartas aleatoriamente.
+    public void barajar()
+    {
+        
+    }
+
+    // Devuelve la siguiente carta que está en la baraja
+    // , cuando no haya más o se haya llegado al final
+    // , se indica al usuario que no hay más cartas.
+    public void siguienteCarta()
+    {
+        
+    }
+
+    // Indica el número de cartas que aún puede repartir.
+    public void cartasDisponibles()
+    {
+        
+    }
+
+    // Dado un número de cartas que nos pidan
+    // , devolveremos ese número de cartas. 
+    // En caso de que haya menos cartas que las pedidas
+    // , no devolveremos nada pero deberemos indicarlo.
+    public void darCartas()
+    {
+        
+    }
+
+    // Mostramos aquellas cartas que ya han salido
+    // , si no ha salido ninguna habrá que indicarlo.
+    public void cartasMonton()
+    {
+        
+    }
+
+    // Muestra todas las cartas hasta el final. 
+    // Es decir, si se saca una carta y luego se llama al método
+    // , éste no mostrara esa primera carta.
+    public void mostrarBaraja()
+    {
+        
+    }
+    // _________________________________________
+    // Métodos Privados
+}
---
+++ 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/PartidaMaximos.java
@@ -0,0 +1,25 @@
+// Programar también una clase PartidaMaximos 
+// que reparta todas las cartas entre cuatro jugadores
+// , y les ponga a jugar de manera que 
+// en cada ronda del juego cada jugador echará una carta
+// , ganando la ronda el jugador que haya echado la carta más alta. 
+
+// El juego se acaba cuando los jugadores se quedan sin cartas. 
+
+// Gana la partida el jugador que haya ganado más rondas. 
+
+// En cada ronda se van rotando los turnos de juego de los jugadores
+// , esto nos permite asegurar que ante una misma puntuación 
+// gane un único jugador. 
+
+// Al finalizar el juego mostrar un mensaje indicando el jugador ganador.
+
+public class PartidaMaximos
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("\n_______________________START\n");
+
+        System.out.println("\n_______________________END\n");
+    }
+}
