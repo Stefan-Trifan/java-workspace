877b92260ec093d8eb521cee9e43942103469dc5
Stefan <trfstephan@gmail.com>
2026-02-10 10:37:28 +0100

    cambios

--- 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/Jugador.java
+++ 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/Jugador.java
@@ -1,22 +1,38 @@
+import java.util.ArrayList;
+
 public class Jugador
 {
     // _________________________________________
-
+    // _______________________________ Atributos
+    private ArrayList<Carta> misCartas;  
+    private String nombre;
     // _________________________________________
-    // ___________________ Constructor Principal 
-
+    // ___________________ Constructor Principal
+    public Jugador()
+    {
+        misCartas = new ArrayList<>();
+    }
     // _________________________________________
-    // ________________ Constructores Auxiliares 
-
+    // ________________ Constructores Auxiliares
     // _________________________________________
     // _________________________________ Getters
-
     // _________________________________________
     // _________________________________ Setters
-
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
     // _________________________________________
     // ________________________ Métodos Públicos
+    public void addCarta(Carta carta)
+    {
+        misCartas.add(carta);
+    }
 
+    public Carta darCarta()
+    {
+        return misCartas.remove(0);
+    }
     // _________________________________________
-    // ________________________ Métodos Privados
+    // ________________________ Métodos Privados 
 }
--- 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/PartidaMaximos.java
+++ 1_utad_programacion_objetos/2_clases/4_practica3_baraja_de_cartas/src/PartidaMaximos.java
@@ -21,13 +21,38 @@ public class PartidaMaximos
         System.out.println("\n_______________________START\n");
 
         Mazo miMazo = new Mazo();
-
         miMazo.barajar();
-        TestMazo.mostrarCartasDisponibles(miMazo);
 
-        miMazo.darCartas(10);
-        TestMazo.mostrarCartasDadas(miMazo);
-        TestMazo.mostrarCartasDisponibles(miMazo);
+        // Creamos 4 jugadores
+        Jugador[] misJugadores = new Jugador[4];
+
+        for(int i = 0; i < misJugadores.length; i++)
+        {
+            misJugadores[i] = new Jugador();
+        }
+
+        misJugadores[0].setNombre("Steve");
+        misJugadores[1].setNombre("Valen");
+        misJugadores[2].setNombre("Diego");
+        misJugadores[3].setNombre("Andres");
+
+        // Repartimos las cartas entre los cuatro jugadores. 
+        // 10 cartas cada uno
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < misJugadores.length; j++)
+            {
+                misJugadores[j].addCarta(miMazo.siguienteCarta());
+            }
+        }
+
+        // Jugamos 10 rondas. Cada ronda un jugador echa una carta
+        // Gana la ronda el jugador con la carta mas alta
+        // Se termina el juego cuando los jugadores se queden sin cartas
+        // Jugamos 10 rondas
+
+
+        
 
         System.out.println("\n_______________________END\n");
     }
