a0c7c7960764ff6c05e92489a3d6dbc5856dae4a
Stefan <trfstephan@gmail.com>
2026-02-26 10:07:35 +0100

    update

---
+++ 2_utad_temario_patrones_diseno/T02_strategy/2_ejs/e10_/src/CalculaArea.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CalculaArea
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Elige el área que desea calcular:");
+        System.out.println("1.Cuadrado 2.Círculo");
+
+        int n = sc.nextInt();
+
+        if (n == 1)
+        {
+
+            System.out.println("lado del cuadrado (cms):");
+            int lado = sc.nextInt();
+
+            System.out.println("El área del cuadrado es de "
+                + lado * lado + " cms");
+
+        }
+        else if (n == 2)
+        {
+
+            System.out.println("Radio del círculo (cms):");
+            int radio = sc.nextInt();
+
+            System.out.println("El área del círculo es de "
+                + Math.PI * Math.pow(radio, 2) + " cms");
+
+        }
+        else
+        {
+
+            System.out.println("Argumento no válido.");
+        }
+
+        sc.close();
+    }
+}
