1b9b6de78196e121da3d5dcfa562c3d20df45686
Stefan <trfstephan@gmail.com>
2026-03-09 22:24:37 +0100

    update

--- 2_utad_temario_patrones_diseno/T03_template/1_diapositivas/p16_doctor_/src/ActivityStrategy.java
+++
@@ -1,4 +0,0 @@
-public interface ActivityStrategy
-{
-    void applyTemplateMethod();
-}
--- 2_utad_temario_patrones_diseno/T04_state/3_caso_estudio/src/Main.java
+++ 2_utad_temario_patrones_diseno/T04_state/3_caso_estudio/src/Main.java
@@ -1,13 +1,8 @@
-// Configurar el sistema y probarlo.
-
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("\n_______________________START\n");
-
-        // todo
-        Doctor miConsulta = new FongDoctor();
 
         System.out.println("\n_______________________END\n");
     }
