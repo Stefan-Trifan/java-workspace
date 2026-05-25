b72f32065069b286997d19dd160d50ecc7480e51
Stefan <trfstephan@gmail.com>
2026-05-25 08:32:20 +0200

    update

--- 2_utad_temario_patrones_diseno/T10_adapter/extra/02_entendiendo_adapter/src/Main.java
+++ 2_utad_temario_patrones_diseno/T10_adapter/extra/02_entendiendo_adapter/src/Main.java
@@ -4,6 +4,33 @@ public class Main
     {
         System.out.println("\n_______________________START\n");
 
+        OldPhoneCharger oldCharger = new OldPhoneCharger();
+
+        
+
         System.out.println("\n_______________________END\n");
+    }
+}
+
+class OldPhoneCharger
+{
+    // ------------------------ Métodos Públicos
+    public void connectOldCharger()
+    {
+        System.out.println("Cargando teléfono con cargador antiguo...");
+    }
+}
+
+interface OldToNewAdapter
+{
+
+}
+
+class NewPhoneCharger
+{
+    // ------------------------ Métodos Públicos
+    public void connectCharger()
+    {
+        System.out.println("Cargando teléfono con cargador nuevo...");
     }
 }
